3b3a6cd65d6ed4feaca674aae125d40f5176afc2
muhammednasimda <muhammednasimda@gmail.com>
2021-08-02 07:03:42 -0700

    first

---
+++ index.js
@@ -0,0 +1,20 @@
+// install express with `npm install express`
+const express = require("express");
+const { getUserProfileInfo } = require("tiktok-scraper");
+const app = express();
+
+app.get("/", async (req, res) => {
+  try {
+    const userMeta = await getUserProfileInfo("tiktok", {});
+    console.log(userMeta);
+    res.send(userMeta);
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
+});
+
+// export 'app'
+// module.exports = app;
+
+app.listen(process.env.PORT || 3000);
